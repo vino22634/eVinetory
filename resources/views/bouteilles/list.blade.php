760e64900a4cd23de21943c361461ec49dceda7b
fhmaisonneuve <e2296133@cmaisonnuve.qc.ca>
2023-10-24 14:14:05 -0400

    Vue Bouteilles (et nouvelle route(pour test): \bouteilles

---
+++ resources/views/bouteilles/list.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste des Bouteilles</title>
+</head>
+
+<body>
+    <h1>Liste des Bouteilles</h1>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nom</th>
+                <th>Image</th>
+                <th>Code SAQ</th>
+                <th>Pays</th>
+                <th>Description</th>
+                <th>Prix SAQ</th>
+            </tr>
+        </thead>
+        <tbody> @foreach($bouteilles as $bouteille) <tr>
+                <td>{{ $bouteille->id }}</td>
+                <td>{{
+    $bouteille->nom }}</td>
+                <td>
+                    @if($bouteille->image)
+                        <img src="{{ $bouteille->image }}" alt="{{ $bouteille->nom }}" width="100">
+                    @else
+                        Pas d'image
+                    @endif
+                </td>
+                <td>{{ $bouteille->code_saq }}</td>
+                <td>{{ $bouteille->pays }}</td>
+                <td>{{ $bouteille->description }}</td>
+                <td>{{ $bouteille->prix_saq }} €</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BouteilleController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +29,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/bouteilles', [BouteilleController::class, 'index'])->name('bouteilles.list');
+
 
 require __DIR__ . '/auth.php';
