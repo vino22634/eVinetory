079edfa074d7467647c26f128ccbe9f4e62a711c
bouryfaye <boury.faye.edu@gmail.com>
2023-11-07 11:54:07 -0500

    Mot de passe oublié

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,9 @@
+@extends('layouts/auth')
+@section('title', 'Envoi du lien de récupération de mot de passe')
+@section('content')
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __("Vous avez oublié votre mot de passe? Pas de problème. Indiquez-nous juste votre adresse courriel et nous vous enverrons un lien pour réinitialiser votre mot de passe et vous permettre d'en choisir un nouveau.") }}
     </div>
 
     <!-- Session Status -->
@@ -10,16 +13,17 @@
         @csrf
 
         <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
+        <div class="input-label mt-4">
+            <x-input-label for="email" :value="__('Courriel')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Lien Courriel pour réinitialiser votre mot de passe') }}
             </x-primary-button>
         </div>
     </form>
 </x-guest-layout>
+@endsection
