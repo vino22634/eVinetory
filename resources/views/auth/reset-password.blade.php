5e6efed6ff690940c242a01fdd8e896f315cdcc5
Michijoe <e2296540@cmaisonneuve.qc.ca>
2023-11-10 11:10:25 -0500

    ajout classes form communes pages auth

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -9,24 +9,24 @@
         <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
         <!-- Email Address -->
-        <div class="input-label">
+        <div class="form_element">
             <x-input-label for="email" :value="__('Courriel')" />
-            <x-text-input id="email" class="text_input" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
+            <x-text-input id="email" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="auth_error_msg"/>
         </div>
 
         <!-- Password -->
-        <div class="input-label">
+        <div class="form_element">
             <x-input-label for="password" :value="__('Mot de passe')" />
-            <x-text-input id="password" class="text_input" type="password" name="password" required autocomplete="new-password" />
+            <x-text-input id="password" type="password" name="password" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password')" class="auth_error_msg" />
         </div>
 
         <!-- Confirm Password -->
-        <div class="input-label">
+        <div class="form_element">
             <x-input-label for="password_confirmation" :value="__('Retapez votre mot de passe')" />
 
-            <x-text-input id="password_confirmation" class="text_input"
+            <x-text-input id="password_confirmation"
                                 type="password"
                                 name="password_confirmation" required autocomplete="new-password" />
 
