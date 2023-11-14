7819715ff68dc558ee5dce74b377d78f4aff70cf
Alain Théroux <alain@theroux.info>
2023-11-14 14:27:03 -0500

    [En cours Favoris] Création d'un contrôleur Favorites (vide) et d'une copie d'app.blade pour tester

---
+++ resources/views/layouts/app.blade_favTest.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="eVinetory, collectionez les saveurs et remplissez votre celliers de vins en ligne dès maintenant. ">
+
+    <title>{{ config('app.name') }} - @yield('title')</title>
+
+    <!-- Styles -->
+    <link href="/css/app.css" rel="stylesheet">
+    <link href="/css/components/navBar.css" rel="stylesheet">
+
+    <!-- Script JS -->
+    <!-- <script src="/js/navBar.js" defer></script> -->
+
+</head>
+
+<body>
+    <div class="container">
+
+        <!-- Nav haut -->
+        <header>
+            <h1>eVinetory</h1>
+        </header>
+
+        <!-- Nav Bar -->
+        <nav id="navbar">
+            <a href="{{route('bouteilles.list')}}" class="{{ request()->is('bouteille*') ? 'active':'' }}">
+                <x-svg.bottlesIcon />
+                <p>Bouteilles</p>
+            </a>
+            <a href="{{route('celliers.index')}}" class="{{ request()->is('cellier*') ? 'active':'' }}">
+                <x-svg.cellarIcon />
+                <p>Celliers</p>
+            </a>
+            <a href="{{route('cellier.show', $cellier->id)}}">
+            <x-svg.heartIcon />
+                <p>Favoris</p>
+            </a> 
+
+                        <!-- Mène à ce qui ressemble à un cellier, mais c'est ramasse-coeurs
+                <a href="" style="pointer-events: none">
+                <x-svg.heartIcon />
+                <p>Favoris</p>
+            </a> -->
+           
+            <a href="" style="pointer-events: none">
+                <x-svg.cartIcon />
+                <p>Achats</p>
+            </a>
+
+                        <!-- Mène à ce qui ressemble à un cellier, mais c'est ramasse-coeurs
+                <a href="" style="pointer-events: none">
+                <x-svg.cartIcon />
+                <p>Achats</p>
+            </a> -->
+
+            <a href="" style="pointer-events: none">
+                <x-svg.profilIcon />
+                <p>Profil</p>
+            </a>
+        </nav>
+
+        <!-- Page Content -->
+        <main>
+            @yield('content')
+        </main>
+
+        <footer></footer>
+    </div>
+</body>
+
+</html>
