03a678be5fea5084e1f6c59fa0a2e7d432c063b1
fhmaisonneuve <e2296133@cmaisonnuve.qc.ca>
2023-11-15 08:54:55 -0500

    Bouteillecellier:  Gestion ajout et delete( maintenant avec refresh)

--- resources/views/components/bouteilles/cardBouteilleInventory-component.blade.php
+++ resources/views/components/bouteilles/cardBouteilleInventory-component.blade.php
@@ -7,6 +7,7 @@
         background-color: #f0f0f0;
         color: #ccc;
     }
+
 </style>
 
 <head>
@@ -20,20 +21,23 @@
             <div id="bouteille-prix">Quantité dans le cellier: {{ $bouteilleCellier->cellier->name }}</div>
         </div>
 
-        <!-- Quantité -->
-        <div class="cardBouteilleCellier-quantity" class="icons">
-            <div class="number-input">
-                <input type="number" min="0" value="{{ $bouteilleCellier->quantite }}" id="bouteilleCellierAmount"
-                    data-id="{{ $bouteilleCellier->id }}"
-                    onchange="bouteilleCellier_saveAmount(this, '{{ $bouteilleCellier->id }}')" />
-            </div>
-        </div>
+
         @if( $bouteilleCellier->id )
+            <!-- Quantité -->
+            <div class="cardBouteilleCellier-quantity" class="icons">
+                <div class="number-input">
+                    <input type="number" min="0" value="{{ $bouteilleCellier->quantite }}" id="bouteilleCellierAmount"
+                        data-id="{{ $bouteilleCellier->id }}"
+                        onchange="bouteilleCellier_saveAmount(this, '{{ $bouteilleCellier->id }}')" />
+                </div>
+            </div>
             <img src="/img/icons/delete.svg" alt="supprimer" class="icons"
-                onclick="bouteilleCellier_delete(this, '{{ $bouteilleCellier->id }}')">
+                onclick="bouteilleCellier_delete(this, '{{ $bouteilleCellier->id }}', 
+                '{{ $bouteilleCellier->id_bouteille }}')">
 
         @else
-            <img src="/img/icons/bouteilles/cellier@2x.png" alt="supprimer" class="icons_action" onclick="bouteilleCellier_add(this, '{{ $bouteilleCellier->id }}', '{{ $bouteilleCellier->id_bouteille }}',
+
+            <img src="/img/icons/bouteilles/addToCellier@2x.png" alt="supprimer" class="icons_action" onclick="bouteilleCellier_add(this, '{{ $bouteilleCellier->id }}', '{{ $bouteilleCellier->id_bouteille }}',
      '{{ $bouteilleCellier->cellier->id }}', '{{ $bouteilleCellier->quantite }}')">
         @endif
     </div>
