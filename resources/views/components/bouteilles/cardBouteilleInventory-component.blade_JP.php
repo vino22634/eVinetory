b48e866a77ceade87992c3e7dc6a5176ef4994b1
fhmaisonneuve <e2296133@cmaisonnuve.qc.ca>
2023-11-16 16:25:47 -0500

    Modif css pour InventaireBouteilleCellier

--- resources/views/components/bouteilles/cardBouteilleInventory-component.blade_JP.php
+++ resources/views/components/bouteilles/cardBouteilleInventory-component.blade_JP.php
@@ -16,20 +16,18 @@
 @if( $bouteilleCellier->id )
     <!-- Quantité -->
     <div class="bouteilleCellier_cta">
-        
-        <div class="number-input  cardBouteilleCellier-quantity">
+        <img src="/img/icons/delete.svg" alt="supprimer" title="Supprimer du cellier" class="icons_action"
+                onclick="bouteilleCellier_delete(this, '{{ $bouteilleCellier->id }}', 
+                '{{ $bouteilleCellier->id_bouteille }}')">
+        <div class="number-input">
             <button onclick="decrementValue(this)"></button>
             <input class="quantity" name="quantity" type="number" min="0" value="{{ $bouteilleCellier->quantite }}" id="bouteilleCellierAmount"
                 data-id="{{ $bouteilleCellier->id }}"
                 onchange="bouteilleCellier_saveAmount(this, '{{ $bouteilleCellier->id }}')" />
             <button onclick="incrementValue(this)" class="plus"></button>
         </div>
-        <img src="/img/icons/delete.svg" alt="supprimer" title="Supprimer du cellier" class="icons_action" onclick="bouteilleCellier_delete(this, '{{ $bouteilleCellier->id }}', 
-                '{{ $bouteilleCellier->id_bouteille }}')">
     </div>
 
 @else
-    <button class="button-small button ajout"
-        onclick="bouteilleCellier_add(this, '{{ $bouteilleCellier->id }}', '{{ $bouteilleCellier->id_bouteille }}','{{ $bouteilleCellier->cellier->id }}', '{{ $bouteilleCellier->quantite }}')">Ajouter
-        au cellier</button>
+    <button onclick="bouteilleCellier_add(this, '{{ $bouteilleCellier->id }}', '{{ $bouteilleCellier->id_bouteille }}','{{ $bouteilleCellier->cellier->id }}', '{{ $bouteilleCellier->quantite }}')">Ajouter au cellier</button>
 @endif
