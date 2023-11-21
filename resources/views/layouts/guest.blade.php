5077e21e669955e3adb96df04d69c5428179ea65
Michijoe <e2296540@cmaisonneuve.qc.ca>
2023-11-21 09:10:49 -0500

    remove user scalable no pour accessibilité

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="/img/icons/appmetalogo.svg" type="image/svg">
 
