3e110625234efe19e4a5de031dbac026c4e3856f
EliRaha <102247703+EliRaha@users.noreply.github.com>
2023-11-21 11:45:47 -0500

    modified controller,blade,routes for formulaire de contact

--- app/Http/Controllers/ErrorController.php
+++ app/Http/Controllers/ErrorController.php
@@ -3,22 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail; //pour envoyer courriel
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactFormEmail;
 
 class ErrorController extends Controller
 {
-    //affichage formulaire error
-   public function index(){
-    return view("erreur.index");
-   }
+    // Affichage du formulaire de contact
+    public function index()
+    {
+        return view('erreur.index');
+    }
 
-   public function sendEmail(Request $request){
+    // Traitement de l'envoi d'e-mails
+    public function sendEmail(Request $request)
+    {
+        // Validation des données du formulaire 
+        $request->validate([
+            'subject' => 'required|string|max:255',
+            'body' => 'required|string',
+        ]);
+
+        // Récupération des données du formulaire
         $subject = $request->input('subject');
         $body = $request->input('body');
 
-        // Envoyer courriel logic en utilisant Laravel Mail
-        mail('evinetory@gmail.com', $subject, $body);
+        // Envoi de l'e-mail
+        try {
+            // Remplacez avec vos propres données d'e-mail (destinataire, contenu, etc.)
+            //  mail('evinetory@gmail.com', $subject, $body);
+            Mail::to('evinetory@gmail.com')->send(new ContactFormEmail($subject, $body));
 
-        return redirect()->route('erreur.index')->with('success', 'Courriel envoyé! Merci!');
-   }
+            // Redirection avec un message de succès
+            return redirect()->route('erreur.index')->with('success', 'Courriel envoyé avec succès! Merci!');
+        } catch (\Exception $e) {
+            // En cas d'erreur lors de l'envoi de l'e-mail
+            return redirect()->route('erreur.index')->with('error', 'Une erreur s\'est produite lors de l\'envoi du courriel.');
+        }
+    }
 }
